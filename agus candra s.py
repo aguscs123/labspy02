d16ae0dfe95c1ecd4139bd5a5a42f5e01ac1db32
agusc123 <agusc081@gmail.com>
2020-11-08 21:38:41 +0700

    menambahkan file project

---
+++ agus candra s.py
@@ -0,0 +1,12 @@
+a = input("Masukan nilai a: ")
+b = input("Masukan nilai b: ")
+c = input("Masukan nilai c: ")
+if(a > b ):
+ if(a > c):
+    print("nilai a lebih besar")
+if(b > a):
+ if(b > c):
+     print("nilai b lebih besar")
+if(c > a):
+ if(c > b):
+     print("nilai c lebih besar")
